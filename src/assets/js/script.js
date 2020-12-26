a95c88307e6688e5825df930cee75168970f593c
emorozov28 <evheniimorozov@gmail.com>
2020-12-26 21:47:44 +0200

    init

---
+++ src/assets/js/script.js
@@ -0,0 +1,4 @@
+console.log('script2.js');
+
+const c = (a, b) => a + b;
+console.log(c(2,2));
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './assets/scss/index.scss';
+
+import './assets/js/script.js';
+
+
+console.log('index.js');
+
+// console.log($('h1'));
